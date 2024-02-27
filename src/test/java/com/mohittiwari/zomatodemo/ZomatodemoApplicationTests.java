adcfcd2db1a160b178a31402de82eaa1772448df
Mohit-tiw <mohit.tiw.23@gmail.com>
2024-02-27 16:03:53 +0530

    Initial commit

---
+++ src/test/java/com/mohittiwari/zomatodemo/ZomatodemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohittiwari.zomatodemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZomatodemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
